0060ed383033003237e905a833deebb48638983f
YkunLin <yankunlin2111@gmail.com>
2026-03-20 13:54:30 -0500

    showing enemy damage

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -52,14 +52,28 @@ public class Enemy : MonoBehaviour
     void OnCollisionEnter(Collision coll)
     {
         GameObject otherGO = coll.gameObject;
-        if(otherGO.GetComponent<ProjectileHero>() != null)
+        
+        //Check for collisions with ProjectileHero
+        ProjectileHero p = otherGO.GetComponent<ProjectileHero>();
+        if(p != null)
         {
-            Destroy(otherGO);       //Destroy the Projectile
-            Destroy(gameObject);    //Destroy the Enemy GameObject
+            //only damage this Enemy if it's on screen
+            if (bndCheck.isOnScreen)
+            {
+                //Get the damage amount from the Main WEAP_DICT.
+                health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+                if(health <= 0)
+                {
+                    //Destroy this Enemy
+                    Destroy(this.gameObject);
+                }
+            }
+            //Destroy the ProjectileHero regardless
+            Destroy(otherGO);
         }
         else
         {
-            Debug.Log("Enemy hit by non-ProjectileHero: " + otherGO.name);
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
         }
     }
 }
