75f43eddefd335976d88432c4f4b09f5a0cd0190
YkunLin <yankunlin2111@gmail.com>
2026-03-20 13:02:57 -0500

    shooting revisited

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;  // Enables the loading & reloading of scenes
 public class Main : MonoBehaviour
 {
     static private Main S;      // A private singleton for Main
+    static private Dictionary<eWeaponType, WeaponDefinition> WEAP_DICT;
 
     [Header("Inscribed")]
+    public bool spawnEnemies = true;
     public GameObject[] prefabEnemies;      //Array of Enemy prefabs
     public float enemySpawnPerSecond = 0.5f;    // # Enemies spawned/second
     public float enemyInsetDefault = 1.5f;  //Inset from the sides
     public float gameRestartDelay = 2;
+    public WeaponDefinition[] weaponDefinitions;
 
     private BoundsCheck bndCheck;
 
@@ -24,10 +27,23 @@ public class Main : MonoBehaviour
 
         //Invoke SpawnEnemy() once (in 2 seconds, based on default values)
         Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+
+        //A generic Dictionary with eWeaponType as the key
+        WEAP_DICT = new Dictionary<eWeaponType, WeaponDefinition>();
+        foreach(WeaponDefinition def in weaponDefinitions)
+        {
+            WEAP_DICT[def.type] = def;
+        }
     }
 
     public void SpawnEnemy()
     {
+        //If spawnEnemies is false, skip to the next invoke of SpawnEnemy()
+        if (!spawnEnemies)
+        {
+            Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+            return;
+        }
         //Pick a random Enemy prefab to instantiate
         int ndx = Random.Range(0, prefabEnemies.Length);
         GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
@@ -67,5 +83,16 @@ public class Main : MonoBehaviour
     static public void HERO_DIED()
     {
         S.DelayedRestart();
+    }
+
+    static public WeaponDefinition GET_WEAPON_DEFINITION(eWeaponType wt)
+    {
+        if (WEAP_DICT.ContainsKey(wt))
+        {
+            return(WEAP_DICT[wt]);
+        }
+        //If no entry of the correct type exists in WEAP_DICT, return a new
+        // WeaponDefinition with a type of eWeaponType.none (the default value)
+        return(new WeaponDefinition());
     }
 }
